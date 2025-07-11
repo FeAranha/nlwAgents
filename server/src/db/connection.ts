751aa4c00fd99f275bb50f9f574a6b42d6c66b6e
Felipe Aranha da Costa <felipearanha.c@gmail.com>
2025-07-11 14:25:28 -0300

    ORM Drizzle

--- server/src/db/connection.ts
+++ server/src/db/connection.ts
@@ -1,8 +1,7 @@
-import postgres from "postgres";
-import { env } from "../env.ts";
+import postgres from 'postgres'
+import { env } from '../env.ts'
+import { drizzle } from 'drizzle-orm/postgres-js'
+import { schema } from './schema/index.ts'
 
 export const sql = postgres(env.DATABASE_URL)
-
-const result = await sql`SELECT 'Hello' as message`
-
-console.log(result)
+export const db = drizzle(sql, { schema, casing: 'snake_case' })
