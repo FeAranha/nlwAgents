87dd029b2a9e3820ae76a4ae4a5b3f9ad8489786
Felipe Aranha da Costa <felipearanha.c@gmail.com>
2025-07-11 10:19:53 -0300

    setup: docker default

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,9 +2,10 @@ import { fastify } from "fastify"
 import { fastifyCors } from "@fastify/cors"
 import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from "fastify-type-provider-zod"
 import { env } from "./env.ts"
+import { sql } from "./db/connection.ts"
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
-
+ 
 app.register(fastifyCors, {
     origin: "http://localhost:5173",
 })
